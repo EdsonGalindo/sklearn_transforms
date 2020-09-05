282d92724c1688f27360cf7609d6b49794d1fc5b
Edson Galindo <31747512+EdsonGalindo@users.noreply.github.com>
2020-09-04 22:34:04 -0300

    Update sklearn_transformers.py
    Voltando alteração, pois não havia necessidade no final.

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -6,7 +6,7 @@ class DropColumns(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
         self.columns = columns
 
-    def _fit(self, X, y=None):
+    def fit(self, X, y=None):
         return self
 
     def transform(self, X):
